80f6b0e69e5f10d3f45e9ef44ca5bb52279553f9
FedeSuarez02 <suarezpipi02@gmail.com>
2025-03-03 20:36:42 -0300

    Agrego Cosas

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,42 @@ menuToggle.addEventListener('click', () => {
 });
 
 
+// Para que funcione el carrousel
 
-    
+
+let indice = 0;
+const carousel = document.querySelector(".carousel");
+const imagenes = document.querySelectorAll(".carousel img");
+const totalImagenes = imagenes.length;
+
+// Función para cambiar imagen manualmente
+function cambiarImagen(direccion) {
+    indice = (indice + direccion + totalImagenes) % totalImagenes;
+    actualizarCarrusel();
+}
+
+// Función para actualizar la posición del carrusel
+function actualizarCarrusel() {
+    const desplazamiento = -indice * 100;
+    carousel.style.transform = `translateX(${desplazamiento}%)`;
+}
+
+// Deslizar con el dedo en dispositivos táctiles
+let startX = 0;
+
+carousel.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+});
+
+carousel.addEventListener("touchend", (e) => {
+    let endX = e.changedTouches[0].clientX;
+    if (startX > endX + 50) cambiarImagen(1); // Deslizar a la izquierda
+    else if (startX < endX - 50) cambiarImagen(-1); // Deslizar a la derecha
+});
+
+
+setInterval(() => cambiarImagen(1), 5000);
+
+
+
     
